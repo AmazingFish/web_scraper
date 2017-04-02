85211e35bc8a84b5fe952d75360e221b9080064d
souppower <souppower@goodpatch.com>
2017-04-03 00:00:35 +0900

    output word and definition to a file

--- fetch.sh
+++ fetch.sh
@@ -4,5 +4,5 @@ readonly base='http://ejje.weblio.jp/content/'
 
 cat wordList.txt | while read word; do
     target=`echo $base$word | tr -d '\r\n'`
-    curl -Lso- $target | grep -oP "content-explanation>\K(.*?)(?=<)" 
+    echo "$word\t`curl -Lso- $target | grep -oP "content-explanation>\K(.*?)(?=<)"`" >> out/out.txt
 done
