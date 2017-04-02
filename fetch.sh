d62acb5e33a0055e834456a9d49c368acf287497
souppower <souppower@goodpatch.com>
2017-04-03 00:08:04 +0900

    refactor fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -4,5 +4,6 @@ readonly base='http://ejje.weblio.jp/content/'
 
 cat wordList.txt | while read word; do
     target=`echo $base$word | tr -d '\r\n'`
-    echo "$word\t`curl -Lso- $target | grep -oP "content-explanation>\K(.*?)(?=<)"`" >> out/out.txt
+    definition=`curl -Lso- $target | grep -oP "content-explanation>\K(.*?)(?=<)"`
+    echo "$word\t$definition" >> out/out.txt
 done
